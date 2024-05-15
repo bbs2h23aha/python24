55b83d4d5200a63b3fedf3bd39ba75669960b8ae
bbs2h23aha <sakina.hafez@edu.bib.de>
2024-05-15 11:33:02 +0200

    aufgabe1

---
+++ intro.py
@@ -0,0 +1,34 @@
+#variable
+text = 12
+text = "Hello World!"
+p=print
+
+# Datentypen
+intVar = 1
+floatVar = 2
+boolVar = True
+StringVar = "Hi!"
+StringVar = 'Hello'
+charVar = 'C'
+
+
+#Operatoren
+example = 1+1-2/4*6
+example2 = 2**3 #2hoch3
+example3 = 6//4 #teilen
+example4 = 6%4
+
+#if , for , while
+if text==12:
+    p("ist 12")
+elif text==13:
+    p("ist 13")
+else:
+    p("ist etwas anderes")
+
+
+
+p(text)
+print("Hello World")
+
+input()
